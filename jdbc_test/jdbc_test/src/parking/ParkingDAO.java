16f15c8a9b6c8478456b0f082c5f7cb646036e1f
USER <USER@1.209.180.150>
2024-08-06 12:47:41 +0900

    2024.08.06 주차관리프로그램

--- jdbc_test/jdbc_test/src/parking/ParkingDAO.java
+++ jdbc_test/jdbc_test/src/parking/ParkingDAO.java
@@ -22,33 +22,111 @@ public class ParkingDAO {
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
 
-	public void insertParking(String location, String carNum) {
+	public int insertParking(int location, String carNum) {
+		int resultChk = 0;
 		
-//		for (int i=0; i<parkingNum.length; i++) {
-//			for (int j=0; j<parkingNum[i].length; j++) {
-//				if (location.equals(parkingNum[i][j])) {
-//					parkingSpace[i][j] = carNum;
-//				}
-//			}
-//		}
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(db_url, "root", "1234");
+			if(conn != null) {
+			}
+		}catch(ClassNotFoundException e) {
+			System.out.println("드라이버 로드 실패");
+			e.printStackTrace();
+		}catch(SQLException e) {
+			System.out.println("접속 실패");
+			e.printStackTrace();
+		}
+		
+		try {
+			String sql = "UPDATE tb_parking_info\r\n"
+					+ "SET parking_yn = 'Y',\r\n"
+					+ "	parking_car_number = ?,\r\n"
+					+ "    parking_date = CURRENT_TIMESTAMP()\r\n"
+					+ "where parking_number = ?;";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, carNum);
+			pstmt.setInt(2, location);
+			
+			resultChk = pstmt.executeUpdate();
+			
+		}catch (SQLException e) {
+			// TODO: handle exception
+			System.out.println("error :" + e);
+		}finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null && conn.isClosed()) {
+					conn.close();
+				}
+			
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return resultChk;
 	}
 
-	public boolean deleteParking(String location) {
+	public int deleteParking(int location) {
+int resultChk = 0;
 		
-		boolean flag = true;
-//		
-//		for (int i=0; i<parkingNum.length; i++) {
-//			for (int j=0; j<parkingNum[i].length; j++) {
-//				if (location.equals(parkingNum[i][j]) && parkingSpace[i][j] != null) {
-//					parkingSpace[i][j] = null;
-//					return true;
-//				} else {
-//					flag = false;
-//				}
-//			}
-//		}
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(db_url, "root", "1234");
+			if(conn != null) {
+			}
+		}catch(ClassNotFoundException e) {
+			System.out.println("드라이버 로드 실패");
+			e.printStackTrace();
+		}catch(SQLException e) {
+			System.out.println("접속 실패");
+			e.printStackTrace();
+		}
 		
-		return flag;
+		try {
+			String sql = "UPDATE tb_parking_info\r\n"
+					+ "SET parking_yn = 'N',\r\n"
+					+ "	parking_car_number = NULL,\r\n"
+					+ "    parking_date = CURRENT_TIMESTAMP()\r\n"
+					+ "where parking_number = ?;";
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, location);
+			
+			resultChk = pstmt.executeUpdate();
+			
+		}catch (SQLException e) {
+			// TODO: handle exception
+			System.out.println("error :" + e);
+		}finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null && conn.isClosed()) {
+					conn.close();
+				}
+			
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return resultChk;
+
 	}
 
 	public List<HashMap<String, Object>> selectParkingSpace() {
@@ -58,7 +136,6 @@ public class ParkingDAO {
 			Class.forName(driver);
 			conn = DriverManager.getConnection(db_url, "root", "1234");
 			if(conn != null) {
-				System.out.println("접속성공");
 			}
 		}catch(ClassNotFoundException e) {
 			System.out.println("드라이버 로드 실패");
@@ -90,7 +167,7 @@ public class ParkingDAO {
 				parkingMap.put("parkingX", rs.getInt("parkingX"));
 				parkingMap.put("parkingY", rs.getInt("parkingY"));
 				parkingMap.put("parkingYn", rs.getString("parkingYn"));
-				parkingMap.put("parkingCarNumber", rs.getInt("parkingCarNumber"));
+				parkingMap.put("parkingCarNumber", rs.getString("parkingCarNumber"));
 				
 				parkingList.add(parkingMap);
 			}
@@ -118,6 +195,120 @@ public class ParkingDAO {
 		
 		return parkingList;
 
+	}
+	
+	public HashMap<String, Object> selectParkingInfo(int location){
+		HashMap<String, Object> parkingMap = new HashMap<String, Object>();
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(db_url, "root", "1234");
+			if(conn != null) {
+			}
+		}catch(ClassNotFoundException e) {
+			System.out.println("드라이버 로드 실패");
+			e.printStackTrace();
+		}catch(SQLException e) {
+			System.out.println("접속 실패");
+			e.printStackTrace();
+		}
+		
+		try {
+			String sql = "SELECT  parking_number AS parkingNumber, \r\n"
+					+ "		parking_car_number AS parkingCarNumber, \r\n"
+					+ "		parking_yn AS parkingYn\r\n"
+					+ "FROM tb_parking_info\r\n"
+					+ "where parking_number = ?;";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, location);
+			
+			rs = pstmt.executeQuery();
+			
+			
+			while(rs.next()) {
+				parkingMap.put("parkingNumber", rs.getInt("parkingNumber"));
+				parkingMap.put("parkingCarNumber", rs.getInt("parkingCarNumber"));
+				parkingMap.put("parkingYn", rs.getString("parkingYn"));				
+			}
+
+		}catch (SQLException e) {
+			// TODO: handle exception
+			System.out.println("error :" + e);
+		}finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null && conn.isClosed()) {
+					conn.close();
+				}
+			
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return parkingMap;
+	}
+	
+	public void insertParkingHistory(int location, String carNum, String type) {
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(db_url, "root", "1234");
+			if(conn != null) {
+			}
+		}catch(ClassNotFoundException e) {
+			System.out.println("드라이버 로드 실패");
+			e.printStackTrace();
+		}catch(SQLException e) {
+			System.out.println("접속 실패");
+			e.printStackTrace();
+		}
+		
+		try {
+			String sql = "INSERT INTO tb_parking_history(\r\n"
+					+ "	parking_location,\r\n"
+					+ "    parking_car_number,\r\n"
+					+ "    parking_type,\r\n"
+					+ "    parking_time\r\n"
+					+ ") values(\r\n"
+					+ "	?,\r\n"
+					+ "    ?,\r\n"
+					+ "    ?,\r\n"
+					+ "    current_timestamp()\r\n"
+					+ ");";
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, location);
+			pstmt.setString(2, carNum);
+			pstmt.setString(3, type);
+			
+			pstmt.executeUpdate();
+			
+		}catch (SQLException e) {
+			// TODO: handle exception
+			System.out.println("error :" + e);
+		}finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null && conn.isClosed()) {
+					conn.close();
+				}
+			
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
--- jdbc_test/jdbc_test/src/parking/ParkingServiceImpl.java
+++ jdbc_test/jdbc_test/src/parking/ParkingServiceImpl.java
@@ -18,11 +18,11 @@ public class ParkingServiceImpl implements ParkingService{
 				switch(menu) {
 
 				case 1 :
-					//inParking();
+					inParking();
 					break;
 
 				case 2 :
-					//outParking();
+					outParking();
 					break;
 
 				case 3 :
@@ -40,29 +40,31 @@ public class ParkingServiceImpl implements ParkingService{
 	
 	public int printMenu() {
 		System.out.println("[ 주차장 현황 ]");
-		System.out.println();
 
 		List<HashMap<String, Object>> parkingList = parkingDAO.selectParkingSpace();
 		for(int i = 0; i<parkingList.size(); i++) {
 			int parkingLocationY = Integer.parseInt(parkingList.get(i).get("parkingY").toString());
+			int parkingLocationX = Integer.parseInt(parkingList.get(i).get("parkingX").toString());
 			String parkingYn = parkingList.get(i).get("parkingYn").toString();
 			int parkingNumber = Integer.parseInt(parkingList.get(i).get("parkingNumber").toString());
-			if(parkingLocationY == 1) {
-				System.out.println("");
+			if(parkingLocationX != 1) {
+				if(parkingLocationY == 1) {
+					System.out.println("");
+				}
 			}
+			
 			if("Y".equals(parkingYn)) {
-				if(parkingNumber > 10) {
+				if(parkingNumber >= 10) {
 					System.out.print("■ " + parkingNumber+ " : " + parkingList.get(i).get("parkingYn") + "\t\t");
 				}else {
-					System.out.print("■ " + parkingNumber+ " : " + parkingList.get(i).get("parkingYn") + "\t\t\t");
+					System.out.print("■ " + parkingNumber+  " : " + parkingList.get(i).get("parkingYn") + "\t\t");
 				}
 			}else {
-				if(parkingNumber > 10) {
-					System.out.print("□ " + parkingNumber+ " : " + parkingList.get(i).get("parkingYn") + "\t\t");
+				if(parkingNumber >= 10) {
+					System.out.print("□ " + parkingNumber+ " : " + parkingList.get(i).get("parkingYn") + "\t");
 				}else {
-					System.out.print("□ " + parkingNumber+ " : " + parkingList.get(i).get("parkingYn") + "\t\t\t");
+					System.out.print("□ " + parkingNumber+ "  : " + parkingList.get(i).get("parkingYn") + "\t");
 				}
-				
 			}
 			
 		}
@@ -80,44 +82,66 @@ public class ParkingServiceImpl implements ParkingService{
 		System.out.println("(예시) 주차 공간 선택 - 1");
 
 		int location = sc.nextInt();
-		String locationStr = String.valueOf(location);
-
+		
+		HashMap<String, Object> parkingMap = new HashMap<String, Object>();
+		parkingMap = parkingDAO.selectParkingInfo(location);
 		// 주차 위치 체크
-		// 잘못된 주차위치 선택시 => System.out.println("주차 위치를 잘못 입력하셨습니다.");
-		// 					=> System.out.println("처음부터 다시 시작하세요.");
+		if(parkingMap.get("parkingNumber") == null) {
+			System.out.println("주차 위치를 잘못 입력하셨습니다.");
+			System.out.println("처음부터 다시 시작하세요.");
+			return;
+		}else {
+			if("Y".equals(parkingMap.get("parkingYn").toString())) {
+				System.out.println("이미 다른 차량이 주차되어 있습니다.");
+				System.out.println("처음부터 다시 시작하세요.");
+				return;
+			}else {
+				//정상 주차
+				System.out.print("주차 차량의 번호를 입력해 주세요. (예: 20가1234) : ");
+				String carNum = sc.next();
+
+				System.out.print("차량 번호가 " + carNum + " 맞습니까? (y / n)");
+				String confirm = sc.next();
+
+				if ("y".equals(confirm.toLowerCase())) {
+					int resultChk =0;
+					resultChk = parkingDAO.insertParking(location, carNum);
+					parkingDAO.insertParkingHistory(location, carNum, "I");
+					if(resultChk > 0) {
+						System.out.println("주차가 완료되었습니다 !!");
+					}else {
+						System.out.println("주차 실패 !!");
+					}
+				} else {
+					System.out.println("처음부터 다시 시작하세요.");
+				}
+				
+			}
+		}
 		// 이미 주차된 위치일 경우
 		//  System.out.println("이미 다른 차량이 주차되어 있습니다.");
 		//	System.out.println("처음부터 다시 시작하세요.");
 		
-		//정상 주차
-		System.out.print("주차 차량의 번호를 입력해 주세요. (예: 20가1234) : ");
-		String carNum = sc.next();
-
-		System.out.print("차량 번호가 " + carNum + " 맞습니까? (y / n)");
-		String confirm = sc.next();
-
-		if ("y".equals(confirm)) {
-			// parkingDAO.insertParking();
-			System.out.println("주차가 완료되었습니다 !!");
-		} else {
-			System.out.println("처음부터 다시 시작하세요.");
-		}
+		
 	}
 
 	public void outParking() {
 
 		System.out.print("주차 위치를 입력해 주세요. : ");
-		String locationStr = sc.next();
-		
-		boolean flag = parkingDAO.deleteParking(locationStr);
-		
-		System.out.println(">>>>>>>>>>>>>>>>>>> " + flag);
+		int location = sc.nextInt();
+		int resultChk = 0;
+		 //출차 이력 등록 Start
+		 HashMap<String, Object> parkingMap = new HashMap<String, Object>();
+		 parkingMap = parkingDAO.selectParkingInfo(location);
+		 parkingDAO.insertParkingHistory(location, parkingMap.get("parkingCarNumber").toString(), "O");
+		//출차 이력 등록 End
+		 resultChk = parkingDAO.deleteParking(location);
 		
 		// 비교 대상의 데이터 타입이 boolean(true 혹은 false)일 경우,
 		// if (flag == true) 구문 ==> if (flag)와 동일하게 사용 가능
 		// if (flag == false) 구문 ==> if (!flag)와 동일하게 사용 가능
 		
-		if (flag) {
+		if (resultChk > 0) {
 			System.out.println("출차 완료!! 안녕히 가세요.");
 		} else {
 			System.out.println("주차된 차량이 없습니다 !! 안녕히 가세요.");
